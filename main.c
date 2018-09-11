6cd12b5eed92740cd6b2d4658a31337a127e65d1
chortas <ceci.hortas@gmail.com>
2018-09-11 09:57:42 -0300

    Arreglo problema de \n

--- main.c
+++ main.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <string.h>
 #include <stdlib.h>
 
 void decodificar() {	
@@ -12,32 +13,52 @@ void decodificar() {
 	unsigned char c2mask = 0X2F;
 
 	//Definición de los resultados y variables temporales
-	char buffer[1];
+	char buffer[100];
 	int contador = 0;
 	unsigned char a1, a2, b1, b2, c1, c2;
 
-	while(fgets(buffer, 1, stdin)) {
+	//Definición de la tabla B64
+	char B64[64]={'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 
+	'W', 'X', 'Y', 'Z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u',
+	'v', 'w', 'x', 'y', 'z', '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '+', '/'};
 
-		switch (contador) {
-			case 0:
-				a1 = *buffer & a1mask; //a1 resultado
-				a2 = *buffer & a2mask;
-			case 1:
-				a2 = a2 << 4;
-				b1 = *buffer & b1mask;
-				b1 = b1 >> 4;
-				b1 = b1 & a2; //b1 resultado 
-				b2 = *buffer & b2mask;
-			case 2:
-				b2 = b2 << 2;
-				c1 = *buffer & c1mask;
-				c1 = c1 >> 6;
-				c1 = c1 & b2; //c1 resultado
-				c2 = *buffer & c2mask; //c2 resultado
+	while(fgets(buffer, 100, stdin) != NULL) {
 		
+		//Remover el salto de línea
+		int i = strlen(buffer)-1;
+  		if(buffer[i]=='\n') 
+      	buffer[i] = '\0';
+
+		if(contador == 0) {
+			a1 = *buffer & a1mask; //a1 resultado
+			printf("%d \n", a1);
+			printf("%c", B64[a1]); 
+			a2 = *buffer & a2mask;
+			contador++;
 		}
-		contador = (contador++)%3;
+
+		if(contador == 1) {
+			a2 = a2 << 4;
+			b1 = *buffer & b1mask;
+			b1 = b1 >> 4;
+			b1 = b1 & a2; //b1 resultado
+			printf("%c", B64[b1]); 
+			b2 = *buffer & b2mask;
+			contador++;
+		}
+
+		if(contador == 2) {
+			b2 = b2 << 2;
+			c1 = *buffer & c1mask;
+			c1 = c1 >> 6;
+			c1 = c1 & b2; //c1 resultado
+			printf("%c", B64[c1]);
+			c2 = *buffer & c2mask; //c2 resultado
+			printf("%c", B64[c2]);
+			contador = 0;
+		}
 	}
+	
 
 	// finalizado el ciclo, fijarse si hay que agregar '=' o '=='
 	switch (contador) {
