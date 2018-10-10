56d8c6a79a3f89c93083dc941dce3427aeb50887
julianferres <julianferres@gmail.com>
2018-10-10 20:18:39 -0300

    Lee y escribe de a varios

--- TP1/archivos para debuggear/x.c
+++ TP1/archivos para debuggear/x.c
@@ -8,13 +8,17 @@ int main(void)
 	printf("anteeees\n");
 	fflush(stdout);
 	char a = readc(0);
+	char b = readc(0);
 	printf("char read: %c\n", a);
+	printf("char read: %c\n", b);
 	printf("despueees\n");
 	fflush(stdout);
 
 	printf("anteeees\n");
 	fflush(stdout);
 	write_c(1, a);
+	printf("\n");
+	write_c(1, b);
 	fflush(stdout);
 	printf("\ndespueees\n");
 	fflush(stdout);
