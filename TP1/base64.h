d22719aedf68a0ac0bf77ae647ff625addeba5e4
chortas <ceci.hortas@gmail.com>
2018-10-12 16:36:43 -0300

    pusheo .h bien

--- TP1/base64.h
+++ TP1/base64.h
@@ -4,21 +4,14 @@
 int encode(int infd, int outfd);
 int decode(int infd, int outfd);
 
-char* errmsg[3] = {
-	"Todo OK",
-	"La lectura del archivo no fue exitosa",
-    "La escritura del archivo no fue exitosa"
-};
-
-
-extern char B64[64];
-
-char* errmsg[5] = {
-	"Todo OK",
-	"La lectura del archivo no fue exitosa",
+ char* errmsg[5]= {
+    "Todo OK",
+    "La lectura del archivo no fue exitosa",
     "La escritura del archivo no fue exitosa",
     "Cantidad de bytes inconsistente en el archivo a decodificar",
     "Contenido inconsistente en el archivo a decodificar"
 };
+
+extern char B64[64];
 
 #endif
