e9db5237e9f12e81914b8d1ac004aae78561012b
chortas <ceci.hortas@gmail.com>
2018-10-12 14:39:08 -0300

    Agrego .h con modificaciones

--- TP1/base64.h
+++ TP1/base64.h
@@ -13,4 +13,12 @@ char* errmsg[3] = {
 
 extern char B64[64];
 
+char* errmsg[5] = {
+	"Todo OK",
+	"La lectura del archivo no fue exitosa",
+    "La escritura del archivo no fue exitosa",
+    "Cantidad de bytes inconsistente en el archivo a decodificar",
+    "Contenido inconsistente en el archivo a decodificar"
+};
+
 #endif
